faaf952822937dadb60997f244daf673b16e9dce
lcy <278761411@qq.com>
2016-03-26 06:15:34 +0800

    liferayTest

---
+++ docroot/WEB-INF/src/com/liferay/docs/guestbook/portlet/GuestPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.guestbook.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class GuestPortlet
+ */
+public class GuestPortlet extends MVCPortlet {
+ 
+
+}
